6a3993961f14680a3e4650aaafe9759bbf5f5856
0xdeczi <fenvironment@protonmail.com>
2026-06-16 16:24:44 +0300

    Update step2.lua

--- purpleghost/step2.lua
+++ purpleghost/step2.lua
@@ -1,2 +1,3 @@
 loadstring"loadstring(game:HttpGet(\"https://raw.githubusercontent.com/ohio99z/Flameso/refs/heads/main/Mutationsuldate.txt\", true))()"()
 --https://raw.githubusercontent.com/ohio99z/Flameso/refs/heads/main/Mutationsuldate.txt open this websait
+--upd he updated script
